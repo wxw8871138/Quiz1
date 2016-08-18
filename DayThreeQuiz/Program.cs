5384ede1639ae008c5942c89894e167e7498e325
WangXinwei <WangXinwei@DESKTOP-6VHU3KN>
2016-08-18 20:43:51 +0800

    Add project files.

---
+++ DayThreeQuiz/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayThreeQuiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputCmlength;
+            double cmLength;
+            double inchLength;
+            Console.Write("Please enter the length in cm:");
+            inputCmlength = Console.ReadLine();
+            cmLength = Convert.ToDouble(inputCmlength);
+            inchLength = cmLength / 2.54;
+            Console.WriteLine("{0:#0.000}",inchLength);
+        }
+    }
+}
